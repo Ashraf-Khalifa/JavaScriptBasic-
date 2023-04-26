7e789e744ad956f57b21d4c2577a2ff06789022d
Ashraf-Khalifa <ashrafrkhalifa@gmail.com>
2023-04-26 20:52:07 +0300

    Done

---
+++ script.js
@@ -0,0 +1,132 @@
+console.log(1000/500);
+
+
+
+
+let revenues= 1000,
+expenses= 500
+
+console.log(revenues+expenses);
+
+let rev= 1000,
+expe= 500
+console.log(revenues/2);
+
+let profit= 1000,
+ sales= 500
+ console.log(profit*sales);
+
+
+ let number= (7, 9, 2)
+ console.log(7+9+2/3);
+
+
+
+ let price= 150,
+  discount= 0.30
+  console.log(150 -(150*0.30));
+
+
+  function isAdult( age ){
+    if( age >18 && age <= 30){
+        return true;
+    }else{
+        return false;
+    }
+
+ }
+ console.log( isAdult(20) );
+
+
+
+
+
+ let number1= 2,
+  number2= 3
+console.log(Math.pow(number1,number2));
+
+console.log(10%4);
+
+
+console.log(typeof(100));
+
+console.log(typeof(73.9));
+
+console.log(typeof(NaN));
+
+ console.log(typeof("Water"));
+
+ console.log(typeof(false));
+
+ console.log(typeof(9 != 11));
+
+ console.log("Orang" + "e");
+
+ console.log("Orange" - "s");
+
+console.log("4" + "8");
+
+console.log("name" + 3);
+
+console.log("name" - 3);
+
+console.log(82 * "word");
+
+console.log(1 + "hello");
+
+console.log("hello" + 1);
+
+console.log(1 + true);
+
+console.log("hello" + true);
+
+console.log(typeof (Infinity));
+
+console.log(1 == '1');
+
+console.log(1 === '1');
+
+
+
+
+
+let text = "Welcome to Orange";
+let result = text.toUpperCase();
+console.log(result);
+
+
+let text1 = "to";
+let result1 = text1.toUpperCase();
+console.log(result1);
+
+
+let text2 = "Hello from Orange";
+let result2 = text2.toString();
+console.log(result2);
+
+
+let text3 = "welcome to orange";
+let result3 = text3.toLowerCase();
+console.log(result3);
+
+
+let x= 17
+console.log(x);
+
+console.log(` Welcome to “Orange”`);
+
+
+
+let text4 = "Welcome to Orange Jordan";
+let result4 = text4.toString();
+console.log(result4);
+
+
+const my_string = "Cactus";
+
+let pattern = "c";
+let replacement = "*";
+
+let my_new_string = my_string.replaceAll(pattern,replacement);
+
+console.log(my_new_string);
